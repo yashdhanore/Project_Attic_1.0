1bf23eff3fae434a6f17ec62ce6616ac7c7c2b7c
Yash Dhanore <65765335+yashdhanore@users.noreply.github.com>
2021-06-01 22:42:42 +0200

    Added Interaction - Label displayed when near bed

--- bed.gd
+++ bed.gd
@@ -1,9 +1,12 @@
 extends Interactive
 
+func onEnter():
+	$Label.show()
+	
 func onInput(event: InputEvent):
 	if event.is_action_pressed("action"):
 		print("Hello")
 		
 func onExit():
-	print("Bye")
+	$Label.hide()
 	
