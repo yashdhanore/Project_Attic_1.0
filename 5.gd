87787884b945a8f6bea1a94c2aaa5a1a25ffa8d6
Yash Dhanore <65765335+yashdhanore@users.noreply.github.com>
2021-06-26 12:47:05 +0200

    added dialogues to all objects

--- 5.gd
+++ 5.gd
@@ -4,8 +4,16 @@ func onEnter():
 	$Label.show()
 	
 func onInput(event: InputEvent):
-	if event.is_action_pressed("action"):
-		print("Hello")        #insert dialogue box here
+	if get_node_or_null('DialogNode') == null:
+		if event.is_action_pressed("action") and interacting:
+			get_tree().paused = true  
+			var dialog = Dialogic.start('timeline-7')
+			dialog.pause_mode = Node.PAUSE_MODE_PROCESS
+			dialog.connect("timeline_end",self,'unpause')
+			add_child(dialog)
+
+func unpause(timeline_name):
+	get_tree().paused = false  
 		
 func onExit():
 	$Label.hide()
