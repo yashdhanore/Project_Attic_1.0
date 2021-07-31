fe13ac6c6f8d6f813a2c994ea1f10aeb41f6f1c5
Yash Dhanore <yashdhanore@gmail.com>
2021-07-31 16:33:20 +0200

    FIxed the mom showing up as soon as game starts

--- OtherCharacters/Mom.gd
+++ OtherCharacters/Mom.gd
@@ -5,17 +5,17 @@ var player = null
 var move = Vector2.ZERO
 var target = Vector2(276,326)
 
-func _on_Area2D_area_entered(area):
-	player = get_parent().get_name()
-	
 func _physics_process(delta):
 	move = Vector2.ZERO
 	
-	if player == null:
+	if player == 1519:
 		move = Vector2.ZERO
 	else:
 		move = position.direction_to(target) * speed
-	
-	move = move_and_slide(move) 
+		
+	move = move_and_slide(move)
+	move = Vector2.ZERO
 
 
+func _on_Area2D_area_shape_entered(area_id, area, area_shape, local_shape):
+	player = area_id
