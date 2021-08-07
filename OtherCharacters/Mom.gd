e8f6be0fd8888230a532b3eeb2ddda9cdd9234dd
Yash Dhanore <yashdhanore@gmail.com>
2021-08-07 23:01:29 +0200

    Mom headshot with dialogue

--- OtherCharacters/Mom.gd
+++ OtherCharacters/Mom.gd
@@ -6,11 +6,14 @@ var move = Vector2.ZERO
 onready var anim = get_node("AnimatedSprite/AnimationPlayer")
 var target = Vector2(282,315)
 var play = true
+onready var dialogue = Dialogic.start('walk-in')
 
 func _physics_process(delta):
-	if play and player == 1509:
+	if play and player == 1514:
 		anim.play("enter")
 		play = false
+		add_child(dialogue)
 	
 func _on_Area2D_area_shape_entered(area_id, area, area_shape, local_shape):
 	player = area_id
+	#print(player)
